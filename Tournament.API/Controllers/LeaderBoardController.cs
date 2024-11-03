0455a19c58c55dab05690edff1359cbff3a130d4
KKFerdousi Neela <khaladakhanam.ferdou@petronas.com>
2024-11-03 18:26:15 +0800

    Add project files.

---
+++ Tournament.API/Controllers/LeaderBoardController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tournament.Model.Entities;
+
+namespace Tournament.API.Controllers
+{
+    [Route("api/LeaderBoard")]
+    [ApiController]
+    public class LeaderBoardController : ControllerBase
+    {
+        [HttpGet]
+        public LeaderBoard GetLeaderBoard()
+        {
+            LeaderBoard board = new LeaderBoard
+            {
+                Id = 1,
+                Player = "Maliha",
+                Score = 1,
+                Comment = "Playing"
+            };
+
+            return board;
+        }
+    }
+}
---
+++ Tournament.Model/Entities/LeaderBoard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Model.Entities
+{
+    public class LeaderBoard
+    {
+        public int Id { get; set; }
+        public string Player { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public int Score { get; set; } = 0;
+    }
+}
