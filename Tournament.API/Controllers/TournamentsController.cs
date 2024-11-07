dc4d2af27efe6c46b9a1e6f1c7265326ec497935
KKFerdousi Neela <khaladakhanam.ferdou@petronas.com>
2024-11-07 22:37:55 +0800

    Add tournament api

---
+++ Tournament.API/Controllers/TournamentsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tournament.Model.Entities;
+
+namespace Tournament.API.Controllers
+{
+    [Route("api/tournaments")]
+    [ApiController]
+    public class TournamentsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IList<Tournaments>> Get()
+        {
+            var tournaments = GenerateMockTournaments();
+            return tournaments.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Tournaments> GetTournamentsById(Guid id)
+        {
+            var tournaments = GenerateMockTournaments();
+            var tournament = tournaments.FirstOrDefault(x => x.Id == id);
+
+            if (tournament == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return tournament;
+        }
+
+        private List<Tournaments> GenerateMockTournaments() 
+        { 
+            return new List<Tournaments> { 
+                new Tournaments(new Guid("b3d3452e-6933-49f7-9ce2-5b671c6f7aff"),"Super","A","ABC"),
+                new Tournaments(new Guid("b3d3453e-6933-49f7-9ce2-5b671c6f7aff"),"Premier","AA","AUST"),
+                new Tournaments(new Guid("b3d3454e-6933-49f7-9ce2-5b671c6f7aff"),"Gold","B","MNC"),
+                new Tournaments(new Guid("b3d3455e-6933-49f7-9ce2-5b671c6f7aff"),"Silver","C","XYZ"),
+            };
+        }
+    }
+}
---
+++ Tournament.Model/Entities/Tournaments.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Model.Entities
+{
+    public class Tournaments
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public string? Type { get; set; }
+        public string? Venue { get; set; }
+
+
+        public Tournaments(Guid id, string description, string type, string venue)
+        {
+            Id = id;
+            Description = description;
+            Type = type;
+            Venue = venue;
+        }
+    }
+
+    
+}
